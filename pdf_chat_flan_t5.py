46140fccd40fbd1df4becea76f70ff5ab761a599
Daiaa Kotb <84987850+Daiaa-K@users.noreply.github.com>
2024-08-11 22:18:59 +0300

    Create pdf_chat_flan_t5.py

---
+++ pdf_chat_flan_t5.py
@@ -0,0 +1,64 @@
+import streamlit as st
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorscores import FAIS
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationalBufferMemory
+from langchain.llms import HuggingFacePipeline
+from PyPDF2 import PDFReader
+from transformers import T5Tokenizer, T5ForConditionalGeneration, pipeline
+
+def get_pdf_text(pdf):
+  text = ''
+  pdf_read = PDFReader(pdf)
+  for page in pdf_read.pages:
+    text += page.extract_text()
+  return text
+
+def get_text_chunks(text):
+  text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+  chunks = text_splitter.split_text(text)
+  return chunks
+
+def get_vectorscore(chunks):
+  embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+  vectorscore =  FAISS.from_texts(texts = chunks, embedding = embeddings)
+  return vectorscore
+
+def flan_t5_pipeline():
+  model_id = "google/flan_t5-large"
+  tokenizer = T5Tokenizer.from_pretrained(model_id)
+  model = T5ForConditionalGeneration.from_pretrained(model_id)
+  pipe = pipeline(
+    "text2text-generation",
+    model = model,
+    tokenizer = tokenizer,
+    max_length=512,
+    temperature=0.7,
+    top_p=0.95
+  )
+  return HuggingFacePipline(pipeline = pipe)
+
+def get_conversation_chain():
+  llm = get_t5_pipeline()
+  memory = ConversationalBufferMemory(memory_key='chat_history', return_messages=True)
+  conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+def handle_user_input(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write("Human: ", message.content)
+        else:
+            st.write("AI: ", message.content)
