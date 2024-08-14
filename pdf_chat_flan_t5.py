82b38559cccf8edbf18374adeb6b0a67e6aa3826
Daiaa Kotb <84987850+Daiaa-K@users.noreply.github.com>
2024-08-14 16:18:22 +0300

    Update pdf_chat_flan_t5.py

--- pdf_chat_flan_t5.py
+++ pdf_chat_flan_t5.py
@@ -19,7 +19,7 @@ HF_API_KEY = st.secrets["api_key"]
 
 # Function to process PDF
 def process_pdf(uploaded_file):
-    pdf_reader = PyPDF2.PdfFileReader(uploaded_file)
+    pdf_reader = PyPDF2.PdfReader(uploaded_file)
     text = ""
     for page_num in range(pdf_reader.getNumPages()):
         text += pdf_reader.getPage(page_num).extract_text()
