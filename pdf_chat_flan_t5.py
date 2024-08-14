b611abda96269b0a86ae340e402886880e63dcb7
Daiaa Kotb <84987850+Daiaa-K@users.noreply.github.com>
2024-08-14 03:58:49 +0300

    Update pdf_chat_flan_t5.py

--- pdf_chat_flan_t5.py
+++ pdf_chat_flan_t5.py
@@ -40,7 +40,7 @@ def get_llm_pipeline():
     huggingface_token = st.secrets["api_key"]
     
     llm = HuggingFaceHub(
-        repo_id="meta-llama/Llama-2-7b-chat-hf",
+        repo_id="EleutherAI/gpt-neo-1.3B",
         huggingfacehub_api_token = huggingface_token,
         model_kwargs={
             "temperature": 0.7,
